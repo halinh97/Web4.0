f5ba4dbea72639a19f22a600ceca00dee01c36ed
halinh97 <nhatlinhrma@gmail.com>
2017-01-07 16:03:59 +0700

    FE_4

---
+++ TeckidsHotGirl/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  var itemTemplate = Handlebars.compile($("#item-template").html());
+  $.ajax({
+    tpye : "get",
+    url  : "imagesData.json"
+
+  }).then(function(data) {
+    var itemHtml = itemTemplate(data);
+    $("#item_list").html(itemHtml);
+    $("#item_list").masonry({
+      itemSelector: '.item_container',
+      columnWidth: '.item_container',
+      percentPosition: true
+    })
+  }).fail(function (error) {
+    console.log("Error:  " +error);
+  });
+});
