3e1ca625c4842558be1ac9505778cdc5d6a9c28e
halinh97 <nhatlinhrma@gmail.com>
2017-01-11 07:08:29 +0700

    bai tap ve  nha FE_6

--- TechKidsHotGirl/js/main.js
+++ TechKidsHotGirl/js/main.js
@@ -1,10 +1,28 @@
 var loadedData = [];
-var isRequestingNextPage =false;
+var isRequestingNextPage = false;
+
 $(document).ready(function(){
   var itemTemplate = Handlebars.compile($("#item-template").html());
   var itemModalTemplate = Handlebars.compile($("#item-modal-template").html());
+  $('.page_loading').hide();
 
   requestNextPage(itemTemplate);
+  isRequestingNextPage = true;
+  $.ajax({
+    type  : "get",
+    url   : "imagesData.json"
+  }).then(function(data){
+
+    var itemHtml = $(itemTemplate(data));
+    $("#item_list").append(itemHtml);
+    $('#item_list').masonry({
+      itemSelector: '.item_container',
+      columnWidth: '.item_container',
+      percentPosition: true
+    });
+  }).fail(function(error){
+    console.log(error);
+  });
 
   $('body').on('click', '.plus_button', function(){
     var itemId = $(this).attr('data-item-id');
@@ -17,16 +35,18 @@ $(document).ready(function(){
         break;
       }
     }
-
     $("#item_modal").modal("show");
   });
 
   $(window).on('scroll', function(){
-    if(isRequestingNextPage)return;
+    if(isRequestingNextPage) return;
 
-    if($(window).scrollTop() + window.innerHeight > $(document).height() - 200){
-      requestNextPage(itemTemplate);
-      isRequestingNextPage = true;
+    if( ($(window).scrollTop() + window.innerHeight ) > ( $(document).height() - 200)){
+      $('.page_loading').show();
+      setTimeout(function(){
+        requestNextPage(itemTemplate);
+      }, 1000);
+      isRequestNextPage = true;
     }
   });
 });
@@ -38,16 +58,21 @@ function requestNextPage(itemTemplate){
   }).then(function(data){
     loadedData = loadedData.concat(data.items);
 
-    var itemHtml = itemTemplate(data);
-    $("#item_list").append(itemHtml);
+    var itemHtml = $(itemTemplate(data));
+    $("#item_list").append(itemHtml).append(itemHtml).masonry( 'appended', itemHtml );
+
     // $('#item_list').masonry({
     //   itemSelector: '.item_container',
     //   columnWidth: '.item_container',
     //   percentPosition: true
     // });
+    // $('#item_list').imagesLoaded().progress( function() {
+    // $('#item_list').masonry('layout');
+    // });
   }).fail(function(error){
     console.log(error);
-  }).always(function () {
-      isRequestingNextPage = true;
+  }).always(function(){
+    isRequestingNextPage = false;
+    $('.page_loading').hide();
   });
 }
